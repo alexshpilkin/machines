c8cc95878e7a959cf32938b22a237a6cffb4055d
Alexander Shpilkin <ashpilkin@gmail.com>
2022-07-20 08:06:09 +0300

    Configure binary cache for nixpkgs-unfree

--- modules/generic.nix
+++ modules/generic.nix
@@ -2,6 +2,10 @@
 
 {
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	nix.settings = {
+		substituters = [ "https://nixpkgs-unfree.cachix.org" ];
+		trusted-public-keys = [ "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs=" ];
+	};
 	nixpkgs.overlays = [ fix-gnome-console ];
 
 	# drivers
