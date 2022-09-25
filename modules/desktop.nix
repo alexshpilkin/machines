6835d214fcb3fbf279902cbbf00ebdd7984121b6
Alexander Shpilkin <ashpilkin@gmail.com>
2022-09-25 03:07:32 +0300

    Install NetworkManager on desktops

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -16,6 +16,8 @@
 		wireplumber.enable = true;
 	};
 
+	networking.networkmanager = { enable = true; enableStrongSwan = true; };
+
 	services.xserver.enable = true;
 	services.xserver.displayManager.gdm = { enable = true; wayland = true; };
 	services.xserver.desktopManager.gnome.enable = true;
