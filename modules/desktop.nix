d7d80b1de4a6d58a66e42c89aafcdc83673cbfb5
Alexander Shpilkin <ashpilkin@gmail.com>
2024-04-19 04:25:43 +0300

    Update lockfile to 2024-04-18

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -58,7 +58,7 @@ in {
 
 	services.xserver.enable = true;
 	services.xserver.displayManager.gdm = { enable = true; wayland = true; };
-	services.xserver.displayManager.defaultSession = "gnome";
+	services.displayManager.defaultSession = "gnome";
 	services.xserver.desktopManager.gnome.enable = true;
 	# https://github.com/NixOS/nixpkgs/issues/32580
 	environment.variables.WEBKIT_DISABLE_COMPOSITING_MODE = "1";
